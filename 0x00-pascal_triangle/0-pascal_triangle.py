e004dd80e34a661386f7ce05ceabe1d61ca106e0
Godfred <goddysmart2468@gmail.com>
2024-08-09 19:53:16 +0000

    fixtures

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,20 +11,17 @@ def pascal_triangle(n):
         return []
 
 
-    """Let's initialize an empty resulting array """
     triangle = [[] for _ in range(n)]
 
     for row_index in range(n):
         for col in range(row_index+1):
             if col < row_index:
                 if col == 0:
-                    """ the first column is always set to 1 """
                     triangle[row_index].append(1)
                 else:
                     triangle[row_index].append(
                         triangle[row_index-1][col] + triangle[row_index-1][col-1])
             elif col == row_index:
-                """ the diagonal is always set to 1 """
                 triangle[row_index].append(1)
 
     return triangle
