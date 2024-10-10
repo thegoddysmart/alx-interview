e81ce400e6923ce798c3fb24cbaeae5d65dbf9a7
Godfred <goddysmart2468@gmail.com>
2024-10-10 12:45:45 +0000

    fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 """
-Module to calculate the perimeter of an island in a grid
+Module to calculate the count of an island in a grid
 """
 
 
 def island_perimeter(grid):
     """
-    Returns the perimeter of the island described in grid.
+    Returns the count of the island described in grid.
 
     Args:
         grid (list of list of int): A rectangular grid of
@@ -16,7 +16,7 @@ def island_perimeter(grid):
     Returns:
         int: The perimeter of the island.
     """
-    perimeter = 0
+    count = 0
     row = len(grid)
     col = len(grid[0]) if row else 0
 
@@ -28,7 +28,7 @@ def island_perimeter(grid):
                      for k in idx]
 
             if grid[i][j]:
-                perimeter += sum([1 if not r or not grid[k[0]][k[1]] else 0
+                count += sum([1 if not r or not grid[k[0]][k[1]] else 0
                               for r, k in zip(check, idx)])
 
-    return (perimeter)
+    return (count)
